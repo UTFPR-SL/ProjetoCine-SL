62390f8e52cac82967e86d40bc13fb33df8ad2b3
Vinicius Simoes Vieira <vinicius_vnk@hotmail.com>
2021-07-19 18:56:11 -0300

    Atalização do diario e organização geral dos arquivos

---
+++ generate.bat
@@ -0,0 +1,72 @@
+@echo off
+@SET op=%1
+@SET name=%2
+    
+if "%name%"=="" goto errNome
+if "%op%"=="paginas" goto cop
+if "%op%"=="sections" goto cop
+if "%op%"=="components" goto cop
+goto err
+
+
+:cop
+   
+mkdir src\%op%\%name%
+if "%ERRORLEVEL%"=="1" goto errExist
+echo @import '../%op%/%name%/%name%.css'; >> src\settings\styleImports.css
+echo $.getScript('../../%op%/%name%/%name%.js', function() {}); >> src\settings\javaImports.js
+
+
+echo. > src\%op%\%name%\%name%.js
+
+
+if "%op%"=="paginas" goto pag
+
+echo $(function (^) { $("#%name%"^).load("../../%op%/%name%/%name%.html"^); }^); >> src\settings\%op%Imports.js
+(
+    echo #%name%{
+    echo.
+    echo }
+) > src\%op%\%name%\%name%.css
+(
+    echo ^<div id="%name%"^>
+    echo.
+    echo ^</div^>
+) > src\%op%\%name%\%name%.html
+
+goto end
+
+
+:pag
+copy src\assets\templates\%op%.html src\%op%\%name%\%name%.html
+echo. > src\%op%\%name%\%name%.js
+
+
+
+goto end
+
+
+:err
+    echo ==============================
+    echo            ERRO!
+    echo      Parametro invalido.
+    echo ==============================
+
+goto end
+
+
+:errNome
+    echo ==============================
+    echo            ERRO!
+    echo        Nome em branco.
+    echo ==============================
+
+goto end
+
+:errExist
+    echo ==============================
+    echo            ERRO!
+    echo        Nome ja em uso.
+    echo ==============================
+
+:end
---
+++ src/settings/javaImports.js
@@ -0,0 +1,16 @@
+
+// Não mexer!
+// Imports Necessários
+
+$.getScript('../../settings/sectionsImports.js', function() {}); 
+$.getScript('../../settings/componentsImports.js', function() {}); 
+
+
+
+// Imports dos components e sections
+
+
+$.getScript('../../sections/asd/asd.js', function() {}); 
+$.getScript('../../paginas/index/index.js', function() {}); 
+$.getScript('../../sections/header/header.js', function() {}); 
+$.getScript('../../sections/footer/footer.js', function() {}); 
