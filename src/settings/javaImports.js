6fe22fd637c52d31170530f0a55b61e1f03acc1f
Vinicius Simoes Vieira <vinicius_vnk@hotmail.com>
2021-07-20 14:06:30 -0300

    Renomeando index para home

--- src/settings/javaImports.js
+++ src/settings/javaImports.js
@@ -10,7 +10,6 @@ $.getScript('../../settings/componentsImports.js', function() {});
 // Imports dos components e sections
 
 
-$.getScript('../../sections/asd/asd.js', function() {}); 
-$.getScript('../../paginas/index/index.js', function() {}); 
 $.getScript('../../sections/header/header.js', function() {}); 
 $.getScript('../../sections/footer/footer.js', function() {}); 
+$.getScript('../../paginas/home/home.js', function() {}); 
