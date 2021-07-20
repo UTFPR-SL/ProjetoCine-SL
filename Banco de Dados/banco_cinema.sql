843cf6274e924dd0d0938086ac3f8cf64974d3f3
Vinicius Simoes Vieira <vinicius_vnk@hotmail.com>
2021-07-20 09:16:08 -0300

    ajustes no banco

--- Banco de Dados/banco_cinema.sql
+++ Banco de Dados/banco_cinema.sql
@@ -2,11 +2,31 @@
 
 USE Cinema;
 
-DROP TABLE IF EXISTS SessaoTemLugares;
+DROP TABLE IF EXISTS Ingresso;
 DROP TABLE IF EXISTS Sessoes;
 DROP TABLE IF EXISTS Lugares;
 DROP TABLE IF EXISTS Filmes;
+DROP TABLE IF EXISTS Compras;
+DROP TABLE IF EXISTS Login;
 
+
+CREATE TABLE IF NOT EXISTS Login(
+id int auto_increment NOT NULL,
+usuario varchar (80),
+senha varchar(80),
+adm bool,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS Compras(
+id int auto_increment NOT NULL,
+id_vendedor int,
+cliente varchar (80),
+cfp varchar(80),
+valor int,
+PRIMARY KEY(id),
+CONSTRAINT id_vendedor FOREIGN KEY (id_vendedor) REFERENCES login(id)
+);
 
 CREATE TABLE IF NOT EXISTS Filmes(
 id int auto_increment NOT NULL,
@@ -19,7 +39,7 @@ PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS Sessoes(
-id int auto_increment NOT NULL PRIMARY KEY,
+id int auto_increment NOT NULL,
 id_filme int,
 horario time,
 data date,
@@ -28,6 +48,7 @@ idioma varchar(32),
 sala varchar(16),
 qtd_lugares int,
 id_sessaoLugar int,
+PRIMARY KEY(id),
 CONSTRAINT id_filme FOREIGN KEY (id_filme) REFERENCES Filmes(id)
 );
 
@@ -35,11 +56,13 @@ CREATE TABLE IF NOT EXISTS Lugares(
 cod char PRIMARY KEY
 );
 
-CREATE TABLE IF NOT EXISTS SessaoTemLugares(
+CREATE TABLE IF NOT EXISTS Ingresso(
     id int auto_increment NOT NULL,
     id_sessao int,
     id_lugar varchar(8),
+    id_compra int,
     PRIMARY KEY (id),
     CONSTRAINT id_sessao FOREIGN KEY (id_sessao) REFERENCES Sessoes(id),
-    CONSTRAINT id_lugar FOREIGN KEY (id_lugar) REFERENCES Lugares(cod)
+    CONSTRAINT id_lugar FOREIGN KEY (id_lugar) REFERENCES Lugares(cod),
+	CONSTRAINT id_compra FOREIGN KEY (id_compra) REFERENCES Compras(id)
 );
