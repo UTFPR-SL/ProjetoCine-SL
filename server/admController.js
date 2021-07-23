ebc29b1bfbcd6c29615a7ec5c9fcd0a17fa27ad9
Vinicius Simoes Vieira <vinicius_vnk@hotmail.com>
2021-07-23 14:47:43 -0300

    Criando rota: Criar Sessao

--- server/admController.js
+++ server/admController.js
@@ -1,41 +1,71 @@
 const banco = require("../server/config");
 
-
-
-exports.test = async (req, res) => {
-  console.log("rota test");
+// Função teste / Adiciona usuario
+exports.criarSessao = async (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  var values = [
-    ['John', 'Highway 71', "asd", false],
-    ['Johnie', 'Highway 72', "asds", false]
-  ];
-  let data = '';
-  req.on('data', async chunk => {
-    data += await chunk;
-  })
-  console.log(data); // 'Buy the milk'
-  req.on('end', () => {
-    // console.log(data); // 'Buy the milk'
-    // console.log(JSON.parse(data)); // 'Buy the milk'
-    // console.log(JSON.parse(data).todo); // 'Buy the milk'
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
+  let data = "";
+  req.on("data", (chunk) => {
+    data += chunk;
+  });
 
+  req.on("end", () => {
+    var string = data.replace("[", "");
+    string = string.replace("]", "");
+    var sql = string.split(", ");
 
-    // banco.query("INSERT INTO login (nome, usuario, senha, adm) VALUES ?", [data], function (err, result) {
-    //   if (err) throw err;
-    //   console.log("Number of records inserted: " + result.affectedRows);
-    // });
+    var id_filme = parseInt(sql[0], 10);
+    var sala = parseInt(sql[5], 10);
+    var e3d = false;
+    if (sql[3] == 'true') e3d = true;
 
-
+    banco.query("INSERT INTO Sessao (id_filme, horario, 3d, idioma, sala) VALUES (?)", [[ id_filme, sql[1], e3d, sql[4], sala ]], function (err, result) {
+      if (err) throw err;
+      console.log("Number of records inserted: " + result.affectedRows);
+    });
 
     res.end();
-  })
-  
-  console.log(data);
+  });
 
-
-
-console.log("asdadsadsadsadsa");
 
 };
+
+
+
+
+// // Função teste / Adiciona usuario
+// exports.teste = async (req, res) => {
+//   console.log("rota test");
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+
+//   let data = "";
+//   req.on("data", (chunk) => {
+//     data += chunk;
+//   });
+
+//   req.on("end", () => {
+//     var string = data.replace("[", "");
+//     string = string.replace("]", "");
+//     var sql = string.split(", ");
+
+//     var adm = false;
+//     if (sql[3] == 'true') adm = true;
+
+//     banco.query("INSERT INTO login (nome, usuario, senha, adm) VALUES (?)", [[sql[0], sql[1], sql[2], adm]], function (err, result) {
+//       if (err) throw err;
+//       console.log("Number of records inserted: " + result.affectedRows);
+//     });
+
+//     res.end();
+//   });
+
+
+// };
