9c06648d50c579af477ceeb5230a8e84ea8e4324
Vinicius Simoes Vieira <vinicius_vnk@hotmail.com>
2021-07-23 14:53:02 -0300

    criando gitignore

--- server/admController.js
+++ server/admController.js
@@ -1,41 +1,5 @@
 const banco = require("../server/config");
 
-// Função teste / Adiciona usuario
-exports.criarSessao = async (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-
-  let data = "";
-  req.on("data", (chunk) => {
-    data += chunk;
-  });
-
-  req.on("end", () => {
-    var string = data.replace("[", "");
-    string = string.replace("]", "");
-    var sql = string.split(", ");
-
-    var id_filme = parseInt(sql[0], 10);
-    var sala = parseInt(sql[5], 10);
-    var e3d = false;
-    if (sql[3] == 'true') e3d = true;
-
-    banco.query("INSERT INTO Sessao (id_filme, horario, 3d, idioma, sala) VALUES (?)", [[ id_filme, sql[1], e3d, sql[4], sala ]], function (err, result) {
-      if (err) throw err;
-      console.log("Number of records inserted: " + result.affectedRows);
-    });
-
-    res.end();
-  });
-
-
-};
-
-
-
 
 // // Função teste / Adiciona usuario
 // exports.teste = async (req, res) => {
