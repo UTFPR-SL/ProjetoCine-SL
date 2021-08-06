cf9b218f633ccb6aa597b4606d6e386ab8ca4224
Vinicius Simoes Vieira <vinicius_vnk@hotmail.com>
2021-08-05 23:39:45 -0300

    Criando modal de login

--- src/settings/componentsImports.js
+++ src/settings/componentsImports.js
@@ -4,3 +4,4 @@
 $(function () { $("#botoesADM").load("../../components/botoesADM/botoesADM.html"); }); 
 $(function () { $("#criarSessao").load("../../components/criarSessao/criarSessao.html"); }); 
 $(function () { $("#addFilme").load("../../components/addFilme/addFilme.html"); }); 
+$(function () { $("#login").load("../../components/login/login.html"); }); 
