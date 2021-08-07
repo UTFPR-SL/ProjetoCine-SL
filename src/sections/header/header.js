59ddc7a2333d69595507962f31fb06657ee40a10
Vinicius Simoes Vieira <vinicius_vnk@hotmail.com>
2021-08-06 23:02:38 -0300

    Criação do login e sessão

--- src/sections/header/header.js
+++ src/sections/header/header.js
@@ -1,9 +1,15 @@
- 
-function loginADM(){
-    var login;
-    login = prompt("Entre com a senha de ADM");
-
-    if(login == null) return;
-    else if(login == "admin")    window.location.href = "../../paginas/adm/adm.html"
-    else alert("Senha errada!");
-};
+function loginADM() {
+  if (sessionStorage.getItem("adm") == null) {
+    alert("Faça login para continuar");
+    document.getElementById("login").style.display = "block";
+    return;
+  }
+  if (sessionStorage.getItem("adm") == 0) {
+    alert("Você não tem permissão para acessar essa página!");
+    return;
+  }
+  if (sessionStorage.getItem("adm") == 1) {
+    window.location.href = "../../paginas/adm/adm.html";
+    return;
+  }
+}
--- src/settings/javascript.js
+++ src/settings/javascript.js
@@ -4,90 +4,34 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-  window.onload = async function() {
-    await sleep(100);
-    var page = window.location.href.split('/');
-    document.getElementById('login').style.display='block';
-    
-    if(page[page.length-2] == "adm"){
-        limparTelaADM();
-        botoesFilmes();
+window.onload = async function () {
+  await sleep(100);
+  var page = window.location.href.split("/");
+
+  if (sessionStorage.getItem("user") == null) {
+    document.getElementsByClassName("logged")[0].style.display = "none";
+    document.getElementsByClassName("logged")[1].style.display = "none";
+  } else {
+    document.getElementsByClassName("logged")[0].style.display = "block";
+    document.getElementsByClassName(
+      "logged"
+    )[0].innerHTML = `<a class="nav-link textoItens">Bem Vindo, ${sessionStorage.getItem(
+      "nome"
+    )}!</a>`;
+    document.getElementsByClassName("logged")[1].style.display = "block";
+  }
+
+  if (page[page.length - 2] == "adm") {
+    if (sessionStorage.getItem("adm") != 1) {
+      alert("Você não tem acesso a esta pagina!");
+      window.location.href = "../../paginas/home/home.html";
     }
+
+    limparTelaADM();
+    botoesFilmes();
+  }
 
   if (page[page.length - 2] == "home") {
     listarSessoes();
   }
 };
-
-
-
-
-
-// console.log("asd");
-// console.log(sessionStorage.getItem("teste"));
-// console.log("123");
-// console.log("asd");
-// console.log(sessionStorage.getItem("teste"));
-// sessionStorage.setItem("teste", "testadasso!!!");
-// console.log(sessionStorage.getItem("teste"));
-// console.log("234");
-
-
-
-
-function test() {
-  alert("Agr vai");
-
-  ajax.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      console.log(data);
-    }
-  };
-  var myData = [
-    {
-      usuario: "Bill",
-      nome: "billie",
-      senha: "asd",
-      adm: false,
-    },
-  ];
-  console.log(myData);
-  ajax.open("post", "http://localhost/test", true);
-  ajax.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  ajax.send(JSON.stringify(myData));
-
-  ajax.onreadystatechange = function () {
-    // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
-    if (ajax.readyState == 4 && ajax.status == 200) {
-      var data = ajax.responseText;
-
-      // Retorno do Ajax
-      console.log(data);
-      var resposta = JSON.parse(this.responseText);
-      //   resposta = data;
-      document.getElementById("teste").innerText = resposta[5].cod;
-    }
-  };
-}
-
-function sel() {
-  alert("Agr vai");
-
-  // var resposta;
-
-  ajax.open("GET", "http://localhost/select", true);
-  ajax.send();
-
-  ajax.onreadystatechange = function () {
-    // Caso o state seja 4 e o http.status for 200, é porque a requisiçõe deu certo.
-    if (ajax.readyState == 4 && ajax.status == 200) {
-      var data = ajax.responseText;
-
-      // Retorno do Ajax
-      console.log(data);
-      var resposta = JSON.parse(this.responseText);
-      //   resposta = data;
-      document.getElementById("teste").innerText = resposta[5].cod;
-    }
-  };
-}
